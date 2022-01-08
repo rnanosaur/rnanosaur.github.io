675fbdb9b3baf88bbbc4a54b3c3c71310681056e
Raffaello Bonghi <raffaello@rnext.it>
2022-01-08 09:24:57 +0100

    Minor fixes

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -1,55 +1,16 @@
-window.cookieconsent.initialise(
-    {
-        "palette":
-        {
-            "popup":
-            {
-                "background": "#000"
-            },
-            "button":
-            {
-                "background": "#f1d600"
-            }
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+            "background": "#eb6c44",
+            "text": "#ffffff"
         },
-        "type": "opt-in",
-        "content":
-        {
-            "message": "This website uses cookies to ensure you get the best experience here.",
-            "href": "https://nanosaur.ai/terms/"
-        },
-        onInitialise: function (status) {
-            var type = this.options.type;
-            var didConsent = this.hasConsented();
-            if (type == 'opt-in' && didConsent) {
-                // enable cookies
-                loadGAonConsent();
-                loadGTonConsent();
-            }
-            if (type == 'opt-out' && !didConsent) {
-                // disable cookies
-            }
-        },
-        onStatusChange: function (status, chosenBefore) {
-            var type = this.options.type;
-            var didConsent = this.hasConsented();
-            if (type == 'opt-in' && didConsent) {
-                // enable cookies
-                loadGAonConsent();
-                loadGTonConsent();
-            }
-            if (type == 'opt-out' && !didConsent) {
-                // disable cookies
-            }
-        },
-        onRevokeChoice: function () {
-            var type = this.options.type;
-            if (type == 'opt-in') {
-                // disable cookies
-            }
-            if (type == 'opt-out') {
-                // enable cookies
-                loadGAonConsent();
-                loadGTonConsent();
-            }
+        "button": {
+            "background": "#f5d948"
         }
-    });
+    },
+    "theme": "classic",
+    "content": {
+        "message": "This website uses cookies to ensure you get the best experience here.",
+        "href": "https://nanosaur.ai/terms/"
+    }
+});
